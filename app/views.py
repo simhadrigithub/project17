70ed17d50c9b72303a809b16b61122295266475e
simhadrigithub <murugeshsimhadri88888@gmail.com>
2023-07-28 12:43:41 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 d={'name':'simha','age':21}
 def data_render(request):
     return render(request,'data_render.html',context=d)
+
+d={'a':20,'b':30,'c':50}
+def data_render1(request):
+    return render(request,'data_render1.html',context=d)
--- project17/urls.py
+++ project17/urls.py
@@ -21,4 +21,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('data_render/',data_render,name='data_render'),
+    path('data_render1/',data_render1,name='data_render1'),
 ]
